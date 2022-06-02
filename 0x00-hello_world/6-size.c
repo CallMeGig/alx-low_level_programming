5f19dd7696186952a45bbc059eca495651a27dc3
CallMeGig <ghislain.gman@gmail.com>
2022-06-02 14:26:16 -0700

    Added size file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,12 @@
 
 int main(void)
 {
-	printtf("Size of char: %d bytes(s)\n" sizeOf(char));
-	printtf("Size of char: %d bytes(s)\n" sizeOf(char));
-	printtf("Size of char: %d bytes(s)\n" sizeOf(char));
-	printtf("Size of char: %d bytes(s)\n" sizeOf(char));
-	printtf("Size of char: %d bytes(s)\n" sizeOf(char));
+
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
+
 }

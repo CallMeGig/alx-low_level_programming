d4392802bbfaa4dab8e0f45da615f892dca20237
CallMeGig <ghislain.gman@gmail.com>
2022-06-02 14:05:58 -0700

    Added size file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+/**
+ * main - prints size of various types
+ * Return: 0
+ */
+
+int main(void)
+{
+	printtf("Size of char: %d bytes(s)\n" sizeOf(char));
+	printtf("Size of char: %d bytes(s)\n" sizeOf(char));
+	printtf("Size of char: %d bytes(s)\n" sizeOf(char));
+	printtf("Size of char: %d bytes(s)\n" sizeOf(char));
+	printtf("Size of char: %d bytes(s)\n" sizeOf(char));
+	return (0);
+}

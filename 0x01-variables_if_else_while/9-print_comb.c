d2449408a86c8d5efb84fb91dca68072dd7f35cf
CallMeGig <ghislain.gman@gmail.com>
2022-06-05 16:20:45 -0700

    Added print_comb.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single digit numbers.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(48 + n);
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar('\n');
+		}
+	}
+
+	return (0);
+}

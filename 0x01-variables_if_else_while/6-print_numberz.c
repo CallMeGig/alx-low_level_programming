43215ff112c2df9cf0769d5b6fe6de3444a9bbb0
CallMeGig <ghislain.gman@gmail.com>
2022-06-05 15:30:36 -0700

    Added print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single base 10 digits smaller than 10 starting from 0.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	char n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}

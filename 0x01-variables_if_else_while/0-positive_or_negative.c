17e5a35c0126b7d882f75d1a20e0c8e91b3031a9
CallMeGig <ghislain.gman@gmail.com>
2022-06-05 13:33:34 -0700

    fixed errors found by betty linter

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+* main - Prints whether the number stored in "n" is positive or negative
+*
+* Description: This program assigns a random number to the variable n and
+* prints whether the number is positive, negative or is equal to 0.
+*
+* Return: 0
+*/
 int main(void)
 {
 	int n;
@@ -11,7 +18,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if ((n = 0))
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);

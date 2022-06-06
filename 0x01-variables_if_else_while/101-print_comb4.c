0a25833bec31260939501ef3895878c2a55c2d45
CallMeGig <ghislain.gman@gmail.com>
2022-06-05 17:18:12 -0700

    Added print_comb4.c file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combinations of three digit numbers.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int n = 0;
+	int m;
+	int o;
+
+	while (n < 10)
+	{
+		m = n + 1;
+		while (m < 10)
+		{
+			o = m + 1;
+			while (o < 10)
+			{
+				putchar(48 + n);
+				putchar(48 + m);
+				putchar(48 + o);
+				if ((o * 100 + n * 10 + m) != 789)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				o++;
+			}
+			m++;
+		}
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}

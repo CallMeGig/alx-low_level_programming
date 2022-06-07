b4e1802aa25e08ff23a925832756a3f6c3bc8a57
CallMeGig <ghislain.gman@gmail.com>
2022-06-07 12:53:50 -0700

    Modified alphabet file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * main - Prints the alphabet, in lowercase, followed by a new line.
+ * print_alphabet - Prints the alphabet, in lowercase, followed by a new line.
  *
  * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
-	int i = 0;
+	char c = 'a';
 
-	while (i < 26)
+	while (c <= 'z')
 	{
-		_putchar(48 + i);
+		_putchar(c);
+		c++;
 	}
 
 	_putchar('\n');

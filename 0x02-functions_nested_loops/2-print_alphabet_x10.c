a30c6f4adc8aa125a5d9bd0ad50520a3ba4e4955
CallMeGig <ghislain.gman@gmail.com>
2022-06-07 13:00:09 -0700

    Modified alphabet x10 file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10x the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n');
+	}
+}

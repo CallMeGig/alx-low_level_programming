b9177c1ae8e352d50e1620aaa9a6cc4d70228685
CallMeGig <ghislain.gman@gmail.com>
2022-06-07 19:26:22 -0700

    Added sign.c file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main"
+
+/**
+ * print_sign - Prints the sign of a number.
+ * @n: Number to be checked
+ *
+ * Return: 1 if n is greater than zero
+ * Return: 0 if n is zero
+ * Reutrn: -1 if n is less than zero
+ */
+
+int print_sign(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		i = 1;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		i = 0;
+	}
+	else
+	{
+		_putchar('-');
+		i = -1;
+	}
+
+	return (i);
+}

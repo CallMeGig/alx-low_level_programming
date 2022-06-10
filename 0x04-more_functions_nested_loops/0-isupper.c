22d2a0175a5b7852214e10f273bbd3e5de761944
CallMeGig <ghislain.gman@gmail.com>
2022-06-10 02:11:59 -0700

    Added isupper file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: character to check
+ * Return: 1 if uppercase else 0
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}

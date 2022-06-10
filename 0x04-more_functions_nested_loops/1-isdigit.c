2696a88ef7e2e6291fc7500acbf08a7ae5c8330e
CallMeGig <ghislain.gman@gmail.com>
2022-06-10 02:58:30 -0700

    Added is digit file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit (0 to 9)
+ * @c: character to check
+ * Return: c if digit else 0
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}

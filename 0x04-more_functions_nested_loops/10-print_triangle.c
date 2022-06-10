cd68a4d4955a419e10c0778b3d9907539705584b
CallMeGig <ghislain.gman@gmail.com>
2022-06-10 12:38:23 -0700

    Removed variable initialization from for loop

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,11 +7,11 @@
 
 void print_triangle(int size)
 {
-	int i = 0;
+	int i = 0, j;
 
 	while (i != size)
 	{
-		for (int j = 0; j < size; j++)
+		for (j = 0; j < size; j++)
 		{
 			if (j < size - 1)
 				_putchar(' ');

6f246c9a77dac598782ef0ffc76ec6fa5f7e84bb
CallMeGig <ghislain.gman@gmail.com>
2022-06-10 06:10:58 -0700

    Added print_line file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * print_line - prints '_' n times to draw a line
+ * @n: number of '_'s to print
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n)
+		_putchar('_');
+	_putchar('\n');
+}

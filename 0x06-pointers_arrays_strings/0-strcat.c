44dd2dc028ac59b21ce27304042358523e37b8fa
CallMeGig <ghislain.gman@gmail.com>
2022-06-15 12:28:16 -0700

    First two files added

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: string to be appended on
+ * @src: string to be appended
+ * Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}

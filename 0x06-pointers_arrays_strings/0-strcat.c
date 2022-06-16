a163d4311311b90d69f474ca68ae791643cd40e3
CallMeGig <ghislain.gman@gmail.com>
2022-06-16 11:43:26 -0700

    tired fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,18 +9,24 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
+	int i, j;
+
+	i = 0;
 
 	while (dest[i] != '\0')
+	{
 		i++;
+	}
+
+	j = 0;
 
 	while (src[j] != '\0')
 	{
-		dest[i] = src[j];
-		i++;
+		dest[i + j] = src[j];
 		j++;
 	}
-	dest[i] = '\0';
+
+	dest[i + j] = '\0';
+
 	return (dest);
 }

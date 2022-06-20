7f5d52121afb5987fdf8942e54111b690e2114ca
CallMeGig <ghislain.gman@gmail.com>
2022-06-20 15:17:38 -0700

    initial commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches string for any set of bytes
+ * @s: string to be searched
+ * @accept: accepted bytes to find
+ * Return: pointer to matching byte else null
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+			j++;
+		}
+		i++
+	}
+
+	return (NULL);
+}

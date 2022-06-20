8d77f2201104d7a5d271353220642dd9fd837074
CallMeGig <ghislain.gman@gmail.com>
2022-06-20 15:49:18 -0700

    initial commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string to search
+ * @needle: substring to find
+ * Return: returns pointer to start of substring else null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j, match = 1;
+
+	while (haystack[i] != '\0')
+	{
+		j = i;
+		while (match)
+		{
+			if (haystack[j] != needle[j] 
+				&& needle[j] != '\0')
+				continue;
+			else
+				match = 0;
+		}
+		if (match)
+			return (&haystack[i]);
+		i++;
+	}
+
+	return (NULL);
+}

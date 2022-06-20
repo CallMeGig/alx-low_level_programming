e9f7ccb3d67a97871dcbfeca9ef2b7d208c4a5be
CallMeGig <ghislain.gman@gmail.com>
2022-06-20 15:17:21 -0700

    initial commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to be checked
+ * @accept: array of accepted bytes
+ * Return: no. bytes in s that consists of bytes
+ * only from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j;
+	int matches = 0;
+
+	while (s[i] != '\0')
+	{
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				matches++;
+				break;
+			}
+			if (accept[j + 1] == '\0' && s[i] != accept[j])
+				return (matches);
+		}
+		i++;
+	}
+	return (matches);
+
+}

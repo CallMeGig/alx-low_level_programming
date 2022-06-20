79505934859c934af48b105c8c70b09587299a3a
CallMeGig <ghislain.gman@gmail.com>
2022-06-20 12:57:21 -0700

    added description for return value

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,6 +5,7 @@
  * @dest: memory area to be copied to
  * @src: memory area to be copied from
  * @n: number of bytes to copy
+ * Return: pointer to dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)

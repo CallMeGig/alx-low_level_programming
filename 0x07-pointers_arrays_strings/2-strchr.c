27ce092441c909a1114389dc85fea72bdaae6a3d
CallMeGig <ghislain.gman@gmail.com>
2022-06-20 13:25:44 -0700

    Added strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to be searched
+ * @c: character to be found
+ * Return: pointer to first character occurrence or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+
+	return (NULL);
+}

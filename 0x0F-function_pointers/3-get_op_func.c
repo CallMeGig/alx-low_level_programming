ca85fa4bfd8e17cadc2c3c23b057a399a887498e
CallMeGig <iamgiggveysunn@gmail.com>
2022-07-07 14:50:15 -0400

    modified get op

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "3-calc.h"
 
 /**
@@ -9,18 +10,18 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add};
-		{"-", op_sub};
-		{"*", op_mul};
-		{"/", op_div};
-		{"%", op_mod};
-		{NULL, NULL};
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (ops[i].p != NULL)
 	{
-		if (s == ops[i].op)
+		if (s == ops[i].p)
 			return (ops[i].f);
 		i++;
 	}

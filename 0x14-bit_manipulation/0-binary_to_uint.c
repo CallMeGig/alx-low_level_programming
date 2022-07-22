fdd8c8763ab66966d0ae8d5b521fc7e7799599e9
CallMeGig <iamgiggveysunn@gmail.com>
2022-07-22 14:04:29 -0400

    fixed desc

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 /**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to binary string
- * Return: unsigned int
+ * Returns: unsigned int
  */
 
 unsigned int binary_to_uint(const char *b)

73207a8f410bd1c6b0907ca3e3b4d9912284ff8f
CallMeGig <iamgiggveysunn@gmail.com>
2022-07-25 14:01:01 -0400

    Initial commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads text file and prints to POSIX std output
+ * @filename: file to be read
+ * @letters: number of letters it should read and print
+ * Return: 0 on failure
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n_read, n_write;
+	char buffer;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	n_write = write(STDOUT_FILENO, buffer, n_read);
+	if (n_write == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	close(fd);
+	return (n_read);
+
+}
